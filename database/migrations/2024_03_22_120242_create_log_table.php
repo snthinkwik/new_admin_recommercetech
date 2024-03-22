b2d42a3a452119e9e16c85cfc6b0f93ccd09017c
Devs NerdCloud <devs+moonrail@nerdcloud.co>
2024-03-22 17:37:25 +0530

    create log table

---
+++ database/migrations/2024_03_22_120242_create_log_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('log', function (Blueprint $table) {
+            $table->id();
+            $table->integer('user_id')->nullable();
+            $table->integer('stock_id')->nullable();
+            $table->integer('sale_id')->nullable();
+            $table->longText('content')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('log');
+    }
+};
